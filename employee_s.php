a45765ae57946aba85acb9fb35efb2bce68339d9
ataollah-2001 <ataollah@sabanciuniv.edu>
2021-12-05 15:47:30 +0300

    added select page for employee table

---
+++ employee_s.php
@@ -0,0 +1,75 @@
+<?php
+include "config.php";
+?>
+
+<form action = "employee_select.php" method = "GET">
+    <b>Employee Id:</b>
+    <select name = "eid">
+
+        <?php
+
+        $sql_comment = "SELECT DISTINCT e_id FROM employee ORDER BY e_id";
+        $myresult = mysqli_query($db,$sql_comment);
+        echo "<option value = >" . "any" ."</option>";
+        while($id_rows = mysqli_fetch_assoc($myresult)){
+            $eid = $id_rows["e_id"];
+            echo "<option value = $eid>". $eid . "</option>";
+        }
+
+        ?>
+
+    </select>
+    <br>
+    <b>Name:</b>
+    <select name = "name">
+
+        <?php
+
+        $sql_comment = "SELECT DISTINCT name FROM employee ORDER BY name";
+        $myresult = mysqli_query($db,$sql_comment);
+        echo "<option value = >" . "any" ."</option>";
+        while($id_rows = mysqli_fetch_assoc($myresult)){
+            $name = $id_rows["name"];
+            echo "<option value = $name>". $name . "</option>";
+        }
+
+        ?>
+
+    </select>
+    <br>
+    <b>Age:</b>
+    <select name = "age">
+
+        <?php
+
+        $sql_comment = "SELECT DISTINCT age FROM employee ORDER BY age";
+        $myresult = mysqli_query($db,$sql_comment);
+        echo "<option value = >" . "any" ."</option>";
+        while($id_rows = mysqli_fetch_assoc($myresult)){
+            $age = $id_rows["age"];
+            echo "<option value = $age>". $age . "</option>";
+        }
+
+        ?>
+
+    </select>
+    <br>
+    <b>Salary:</b>
+    <select name = "salary">
+
+        <?php
+
+        $sql_comment = "SELECT DISTINCT salary FROM employee ORDER BY salary";
+        $myresult = mysqli_query($db,$sql_comment);
+        echo "<option value = >" . "any" ."</option>";
+        while($id_rows = mysqli_fetch_assoc($myresult)){
+            $salary = $id_rows["salary"];
+            echo "<option value = $salary>". $salary . "</option>";
+        }
+
+        ?>
+
+    </select>
+    <br> 
+    <button>SELECT</button>
+</form>
---
+++ employee_select.php
@@ -0,0 +1,70 @@
+<?php
+include "config.php";
+
+if (!empty($_GET["eid"])){
+    $eid = $_GET["eid"];
+    
+    $sql_comment = "SELECT * FROM employee WHERE e_id = '".$eid."' ";
+
+    if (!empty($_GET["name"])) {
+        $pro = $_GET["name"];
+        $sql_comment .= " AND name = '".$name."' ";
+    }
+
+    if (!empty($_GET["age"])) {
+        $age = $_GET["age"];
+        $sql_comment .= " AND age = '".$age."' ";
+    }
+
+    if (!empty($_GET["salary"])) {
+        $salary = $_GET["salary"];
+        $sql_comment .= " AND salary = '".$salary."' ";
+    }
+
+    $myresult = mysqli_query($db,$sql_comment);
+
+    while($rows = mysqli_fetch_assoc($myresult)){
+        echo $rows["e_id"] . " - " . $rows["name"] . " - " . $rows["age"] . " - " . $rows["salary"] . "<br>";
+    }
+}
+else {
+    $sql_comment = "SELECT * FROM employee ";
+    $hasWhere = false;
+
+    if (!empty($_GET["name"])) {
+        $pro = $_GET["name"];
+        $sql_comment .= " WHERE name = '".$name."' ";
+        $hasWhere = true;
+    }
+
+    if (!empty($_GET["age"])) {
+        $age = $_GET["age"];
+        if($hasWhere) {
+            $sql_comment .= " AND age = '".$age."' ";
+        }
+        else {
+            $sql_comment .= " WHERE age = '".$age."' ";
+            $hasWhere = true;
+        }
+    }
+
+    if (!empty($_GET["salary"])) {
+        $salary = $_GET["salary"];
+        if($hasWhere) {
+            $sql_comment .= " AND salary = '".$salary."' ";
+        }
+        else {
+            $sql_comment .= " WHERE salary = '".$salary."' ";
+            $hasWhere = true;
+        }
+    }
+
+    $myresult = mysqli_query($db,$sql_comment);
+
+    while($rows = mysqli_fetch_assoc($myresult)){
+        echo $rows["e_id"] . " - " . $rows["name"] . " - " . $rows["age"] . " - " . $rows["salary"] . "<br>";
+    }
+}
+
+
+?>
