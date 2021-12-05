74cc39af80d181a78dc66beccf002b362c97d32a
ataollah-2001 <ataollah@sabanciuniv.edu>
2021-12-05 12:13:01 +0300

    added select page for patient table

---
+++ patient_s.php
@@ -0,0 +1,90 @@
+<?php
+include "config.php";
+?>
+
+<form action = "patient_select.php" method = "GET">
+    <b>Patient Id:</b>
+    <select name = "pid">
+
+        <?php
+
+        $sql_comment = "SELECT DISTINCT p_id FROM patient ORDER BY p_id ";
+        $myresult = mysqli_query($db,$sql_comment);
+        echo "<option value = >" . "any" ."</option>";
+        while($id_rows = mysqli_fetch_assoc($myresult)){
+            $pid = $id_rows["p_id"];
+            echo "<option value = $pid>" . $pid ."</option>";
+        }
+
+        ?>
+
+    </select>
+    <br>
+    <b>Name:</b>
+    <select name = "name">
+
+        <?php
+
+        $sql_comment = "SELECT DISTINCT name FROM patient ORDER BY name ";
+        $myresult = mysqli_query($db,$sql_comment);
+        echo "<option value = >" . "any" ."</option>";
+        while($id_rows = mysqli_fetch_assoc($myresult)){
+            $name = $id_rows["name"];
+            echo "<option value = $name>". $name . "</option>";
+        }
+
+        ?>
+
+    </select>
+    <br>
+    <b>Age:</b>
+    <select name = "age">
+
+        <?php
+
+        $sql_comment = "SELECT DISTINCT age FROM patient ORDER BY age ";
+        $myresult = mysqli_query($db,$sql_comment);
+        echo "<option value = >" . "any" ."</option>";
+        while($id_rows = mysqli_fetch_assoc($myresult)){
+            $age = $id_rows["age"];
+            echo "<option value = $age>". $age . "</option>";
+        }
+
+        ?>
+
+    </select>
+    <br><b>Height:</b>
+    <select name = "height">
+
+        <?php
+
+        $sql_comment = "SELECT DISTINCT height FROM patient ORDER BY height ";
+        $myresult = mysqli_query($db,$sql_comment);
+        echo "<option value = >" . "any" ."</option>";
+        while($id_rows = mysqli_fetch_assoc($myresult)){
+            $height = $id_rows["height"];
+            echo "<option value = $height>". $height . "</option>";
+        }
+
+        ?>
+
+    </select>
+    <br><b>Weight:</b>
+    <select name = "weight">
+
+        <?php
+
+        $sql_comment = "SELECT DISTINCT weight FROM patient ORDER BY weight ";
+        $myresult = mysqli_query($db,$sql_comment);
+        echo "<option value = >" . "any" ."</option>";
+        while($id_rows = mysqli_fetch_assoc($myresult)){
+            $weight = $id_rows["weight"];
+            echo "<option value = $weight>". $weight . "</option>";
+        }
+
+        ?>
+
+    </select>
+    <br>
+    <button>SELECT</button>
+</form>
---
+++ patient_select.php
@@ -0,0 +1,83 @@
+<?php
+include "config.php";
+
+if (!empty($_GET["pid"])){
+    $pid = $_GET["pid"];
+    
+    $sql_comment = "SELECT * FROM patient WHERE p_id = '".$pid."' ";
+
+    if (!empty($_GET["name"])){
+        $name = $_GET["name"];
+        $sql_comment .= " AND name = '".$name."' ";
+    }
+
+    if (!empty($_GET["age"])){
+        $age = $_GET["age"];
+        $sql_comment .= " AND age = '".$age."' ";
+    }
+
+    if (!empty($_GET["height"])){
+        $height = $_GET["height"];
+        $sql_comment .= " AND height = '".$height."' ";
+    }
+    
+    if (!empty($_GET["weight"])){
+        $weight = $_GET["weight"];
+        $sql_comment .= " AND weight = '".$weight."' ";
+    }
+
+    $myresult = mysqli_query($db,$sql_comment);
+
+    while($rows = mysqli_fetch_assoc($myresult)){
+        echo $rows["p_id"] . " - " . $rows["name"] . " - " . $rows["age"] . " - " . $rows["height"] . " - " . $rows["weight"] . "<br>";
+    }
+}
+else {
+    $sql_comment = "SELECT * FROM patient";
+    $hasWhere = false;
+
+    if (!empty($_GET["name"])){
+        $name = $_GET["name"];
+        $sql_comment .= " WHERE name = '".$name."' ";
+        $hasWhere = true;
+    }
+
+    if (!empty($_GET["age"])){
+        $age = $_GET["age"];
+        if($hasWhere) {
+            $sql_comment .= " AND age = '".$age."' ";
+        }
+        else {
+            $sql_comment .= " WHERE age = '".$age."' ";
+        }
+    }
+
+    if (!empty($_GET["height"])){
+        $height = $_GET["height"];
+        if($hasWhere) {
+            $sql_comment .= " AND height = '".$height."' ";
+        }
+        else {
+            $sql_comment .= " WHERE height = '".$height."' ";
+        }
+    }
+    
+    if (!empty($_GET["weight"])){
+        $weight = $_GET["weight"];
+        if($hasWhere) {
+            $sql_comment .= " AND weight = '".$weight."' ";
+        }
+        else {
+            $sql_comment .= " WHERE weight = '".$weight."' ";
+        }
+    }
+
+    $myresult = mysqli_query($db,$sql_comment);
+
+    while($rows = mysqli_fetch_assoc($myresult)){
+        echo $rows["p_id"] . " - " . $rows["name"] . " - " . $rows["age"] . " - " . $rows["height"] . " - " . $rows["weight"] . "<br>";
+    }
+}
+
+
+?>
